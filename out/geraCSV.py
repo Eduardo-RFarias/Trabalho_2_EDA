14805072919996a70bc057c3dc2ce7680213e84f
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2021-05-14 15:41:45 -0300

    diretório corrigido

--- out/geraCSV.py
+++ out/geraCSV.py
@@ -2,12 +2,13 @@ from random import randint
 
 # formato csv: 1230, 129387, 127, 194, 4, 534
 
-arquivo = open('aleatorios.csv', 'w')
+arquivo = open('/out/aleatorios.csv', 'w')
 
 tamanho = int(input('digite quantos números você quer no CSV: '))
 
 for i in range(tamanho):
     arquivo.write(str(randint(-10000, 10000)))
-    if i != tamanho - 1: arquivo.write(', ')
+    if i != tamanho - 1:
+        arquivo.write(', ')
 
 arquivo.close()
